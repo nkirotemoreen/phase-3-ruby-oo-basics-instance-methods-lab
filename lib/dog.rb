f1ba34a12f5c71bbb9e71e057e94596a6ce81ebb
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 01:50:57 +0300

    done

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    #class body
+    #instance method deffination
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+jimmy = Dog.new
+jimmy.bark
+
+snoppy = Dog.new
+snoppy.bark
+snoppy.sit
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    #adding an instance of the class
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts  "The Person is walking"
+    end
+end
+
+moreen = Person.new
+moreen.talk
+moreen.walk
